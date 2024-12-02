220f94efaf6397bf9c050fc35647e5f734985ec3
Jesus Gastelum <gastelumtec21@gmail.com>
2024-12-01 18:28:48 -0700

    feat: devolver id de la caja al registrarla

--- src/core/models/caja-model.ts
+++ src/core/models/caja-model.ts
@@ -1,12 +1,15 @@
 import { Planta } from "../db/entities/planta-entity";
 
 export class Caja {
+  private idCaja: number;
   constructor(
     private readonly kg: number,
     private readonly planta: Planta,
     private readonly fecha: Date,
     private readonly tipo: string
-  ){}
+  ){
+
+  }
 
   public get getKg() {
     return this.kg;
@@ -22,5 +25,9 @@ export class Caja {
 
   public get getTipo() {
     return this.tipo;
+  }
+
+  public set setId(idCaja: number) {
+    this.idCaja = idCaja;
   }
 }
